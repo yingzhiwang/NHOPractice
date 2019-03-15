96bd95b221f0ad3378a82223cf1e9581bd1081c7
xiaopeng.tang <xiaopeng.tang@thoughtworks.com>
2019-03-15 21:16:52 +0800

    init project

---
+++ src/main/java/com/tw/nho/practicesystem/PracticeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tw.nho.practicesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSystemApplication.class, args);
+    }
+
+}
